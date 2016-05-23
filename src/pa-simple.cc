585d05a036359b67abc7eabf542ecf14e5bf36ae
Yannick Adam <yannick.adam@gmail.com>
2016-05-23 16:59:57 +0000

    added index

--- src/pa-simple.cc
+++ src/pa-simple.cc
@@ -1,15 +1,34 @@
 #include <node.h>
+#include <pulse/simple.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 using namespace v8;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
+void Init(const FunctionCallbackInfo<Value>& args) {
 	  Isolate* isolate = Isolate::GetCurrent();
-	    HandleScope scope(isolate);
-		  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+	  HandleScope scope(isolate);
+
+	pa_sample_spec	ss;
+	ss.format = PA_SAMPLE_S16LE;
+	ss.channels = 2;	
+	ss.rate = 44100;
+
+	pa_simple *s;
+	int error;
+
+	s = pa_simple_new(NULL, "npas", PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error);
+
+	int fd = open("/home/root/da.wav", O_RDONLY);
+
+
+
+
+	args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
 }
 
 void init(Handle<Object> exports) {
-	  NODE_SET_METHOD(exports, "hello", Method);
+	  NODE_SET_METHOD(exports, "Init", Init);
 }
 
-NODE_MODULE(hello, init)
+NODE_MODULE(npas, init)
